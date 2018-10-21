aaad4ad4decc07aa72a55ba647fd2dc7bd8b34f4
GuMiner <gus.gran@gmail.com>
2018-10-20 20:56:41 -0700

    Adding useful scripts to verify all my Flickr photos actually exist locally too.

---
+++ Find-MissingFilesByReference.ps1
@@ -0,0 +1,49 @@
+﻿<#
+  Finds all files which don't exist, given a list of filenames (without extensions) and a persistent directory where the files should be stored.
+#>
+
+param(
+    #[Parameter(Mandatory=$true)]
+    [string]
+    $FilenamesFile, # This should be a newline-deliminated list
+
+    #[Parameter(Mandatory=$true)]
+    [string]
+    $PersistentDirectory)
+
+$sourceFiles = [IO.File]::ReadAllLines($FilenamesFile)
+$existingFiles = Get-ChildItem $PersistentDirectory -Recurse
+
+$foundFiles = 0
+$parsedFiles = 0
+Write-Output "Read in $($sourceFiles.Count) file names to search for."
+foreach ($filename in $sourceFiles)
+{
+    # Inefficient, but good enough for the scale of files I'm using.
+    $foundFile = $false
+    foreach ($existingFile in $existingFiles)
+    {
+        if ([IO.Path]::GetFileNameWithoutExtension($existingFile.Name.ToString()).Equals($filename, [StringComparison]::OrdinalIgnoreCase))
+        {
+            $foundFile = $true
+            break
+        }
+    }
+
+    if (-not $foundFile)
+    {
+        Write-Output $filename
+    }
+    else
+    {
+        ++$foundFiles
+    }
+
+    ++$parsedFiles
+    if ($parsedFiles % 50 -eq 0)
+    {
+        Write-Output "-- $parsedFiles of $($sourceFiles.Count)"
+    }
+}
+
+Write-Output "$($sourceFiles.Count) total files, $foundFiles files found."
---
+++ Get-Images.ps1
@@ -0,0 +1,42 @@
+﻿<#
+  Retrieves all Flickr photos for a given user from Flickr, saving their titles in the given output file.
+
+  This is intended for small, personal Flickr sites, so there is no rate-limiting / throttle detection included in this short script.
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$UserId,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$ApiKey,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$OutputFilename)
+
+function ConvertFrom-FlickrJson
+{
+    param($FlikrJson)
+
+    return ConvertFrom-Json $FlikrJson.Substring("jsonFlickrApi(".Length).TrimEnd(')')
+}
+
+[IO.File]::WriteAllText($OutputFilename, "")
+
+$content = Invoke-RestMethod -Method Get -URI "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=$ApiKey&user_id=$UserId&format=json"
+$jsonContent = ConvertFrom-FlickrJson -FlikrJson:$content
+
+Write-Output "Photosets: $($jsonContent.photosets.photoset.Count)"
+foreach ($photoset in $jsonContent.photosets.photoset)
+{
+    Write-Output "Querying $($photoset.title)..."
+    $content = Invoke-RestMethod -Method Get -URI "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=$ApiKey&user_id=$UserId&photoset_id=$($photoset.id)&format=json"
+    $photos = ConvertFrom-FlickrJson -FlikrJson:$content
+
+    Write-Output "Read in $($photos.photoset.photo.Count) photos."
+    foreach ($photo in $photos.photoset.photo)
+    {
+        [IO.File]::AppendAllText($OutputFilename, "$($photo.title)$([Environment]::NewLine)")
+    }
+}
+
+Write-Output "Done."
