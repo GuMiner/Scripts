0776628eaf49d44767d89d1b3ece49964eb5e835
GuMiner <gus.gran@gmail.com>
2020-11-27 10:16:40 -0800

    Add a portrait video converter (lazily) and fix some reshuffling of the scripts breaking them.

---
+++ SlowMotionVideo/HEVC-to-MP4-Portrait.bat
@@ -0,0 +1,2 @@
+C:\Python37\python.exe %0\..\scripts\HEVC-to-MP4-Portrait.py %*
+pause
--- SlowMotionVideo/scripts/HEVC-to-MP4-Portrait.py
+++ SlowMotionVideo/scripts/HEVC-to-MP4-Portrait.py
@@ -15,7 +15,7 @@ cmd = 'C:\\users\\Gustave\\Desktop\\Programs\\HandbrakeCLI\\HandbrakeCLI.exe'
 input = sys.argv[1]
 output = f'{os.path.splitext(input)[0]}.mp4'
 
-args = [cmd, '--preset-import-file', 'HEVC-to-MP4-preset.json', '--preset',  '1080p-Landscape-HEVC-converter', '--input', input, '--output', output]
+args = [cmd, '--preset-import-file', 'HEVC-to-MP4-preset-Portrait.json', '--preset',  '1080p-Portrait-HEVC-converter', '--input', input, '--output', output]
 print(args)
 process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for line in iter(process.stdout.readline, b''):
