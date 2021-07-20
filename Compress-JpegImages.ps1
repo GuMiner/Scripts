0b1340b9a2a78bcfafde3e4f4fb087c8cdd6caad
GuMiner <gus.gran@gmail.com>
2021-07-19 19:16:32 -0700

    Update compression to also convert HEIC to JPG and compress them, which saves about 30% storage space with minimal quality reduction.

--- Compress-JpegImages.ps1
+++ Compress-JpegImages.ps1
@@ -1,21 +1,22 @@
 <#
 .DESCRIPTION
 Runs jpeg-recompress (https://github.com/danielgtaylor/jpeg-archive) on a folder of JPEG files.
+Converts HEIC files to JPG too, as that saves ~1 MB per file.
 
 .PARAMETER Folder
 The folder with files to process
 
 .PARAMETER JpegRecompressPath
 The path to 'jpeg-recompress.exe' to run to recompress files.
-#>
-param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $Folder,
 
-    [Parameter(Mandatory=$true)]
-    [string]
-    $JpegRecompressPath)
+.PARAMETER ImageMagickPath
+The path to 'magick.exe' to convert HEIC files to JPG files.
+#>
+$Folder = "C:\Users\Gustave\Desktop\2021 Enumclaw Day"
+
+$JpegRecompressPath = "C:\Users\Gustave\Desktop\Programs\jpeg-archive\jpeg-recompress.exe"
+
+$ImageMagickPath = "C:\Users\Gustave\Desktop\Programs\ImageMagick\magick.exe"
 
 foreach ($file in [IO.Directory]::GetFiles($Folder))
 {
@@ -23,5 +24,15 @@ foreach ($file in [IO.Directory]::GetFiles($Folder))
     {
         Write-Output "Processing $file ..."
         & $JpegRecompressPath -q high $file $file
+    }
+    elseif ($file.EndsWith("heic", [StringComparison]::OrdinalIgnoreCase))
+    {
+        $jpgFilename = "$([IO.Path]::GetDirectoryName($file))\$([IO.Path]::GetFileNameWithoutExtension($file)).jpg"
+
+        Write-Output "Converting $file ..."
+        & $ImageMagickPath convert $file $jpgFilename
+
+        Write-Output "Processing $jpgFilename ..."
+        & $JpegRecompressPath -q high $jpgFilename $jpgFilename
     }
 }
